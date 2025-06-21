d3f4b0fa75ed1e5fc555ebf9ac468935b00a7505
Владимир Ивахненко <ivahnenkovm@ugpa.ru>
2025-06-21 16:16:55 +0300

    fixed Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.example", "org.audio", "org.textsearch"})
+@ComponentScan(basePackages = {"org.example", "org.audio", "org.textsearch", "org.playlists"})
 public class Main {
 
     @RequestMapping("/")
